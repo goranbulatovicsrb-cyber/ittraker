ef7bf5a007b70435d2a968b16ef3ac8dddfcda7c
goranbulatovicsrb-cyber <goranbulatovicsrb@gmail.com>
2026-03-11 22:45:52 +0100

    Add files via upload

---
+++ src/clientspage.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "database.h"
+#include "paginationwidget.h"
+#include <QWidget>
+#include <QTableWidget>
+#include <QLineEdit>
+#include <QLabel>
+#include <QPushButton>
+
+class ClientsPage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ClientsPage(QWidget* parent = nullptr);
+    void refresh();
+
+private slots:
+    void onAdd();
+    void onEdit();
+    void onDelete();
+    void onExportCsv();
+    void onFilter(const QString& text);
+    void onSelectionChanged();
+    void onPageChanged();
+
+private:
+    void buildUi();
+    void populateTable();
+    int  selectedClientId() const;
+
+    QList<Client>     m_filtered;
+    QTableWidget*     m_table    = nullptr;
+    QLineEdit*        m_search   = nullptr;
+    QPushButton*      m_btnEdit  = nullptr;
+    QPushButton*      m_btnDel   = nullptr;
+    QLabel*           m_lblCount = nullptr;
+    PaginationWidget* m_pager    = nullptr;
+
+    QList<Client> m_allClients;
+};
